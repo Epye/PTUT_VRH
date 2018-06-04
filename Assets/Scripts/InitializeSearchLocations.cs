5be83325c8131c2942498e5f4ef61e764d6d5cd2
jmodoux <josua.modoux@gmail.com>
2018-06-04 17:11:02 +0200

    add all toggles by scripts

---
+++ Assets/Scripts/InitializeSearchLocations.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitializeSearchLocations : MonoBehaviour {
+    public GameObject content;
+    public GameObject toggle;
+
+    private static ArrayList locations = new ArrayList(new string[] { "Ain", "Aisne", "Allier", "Alpes de Haute-Provence", "Alpes-Maritimes", "Ardèche", "Ardennes", "Ariège", "Aube", "Aude", "Aveyron", "Bas-Rhin", "Bouches du Rhône", "Calvados", "Cantal", "Charente", "Charente Maritime", "Cher", "Corrèze", "Corse du Sud", "Côte d'Or", "Côtes d'Armor", "Creuse", "Deux-Sèvres", "Dordogne", "Doubs", "Drôme", "Essonne", "Eure", "Eure-et-Loir", "Finistère", "Gard", "Gers", "Gironde", "Haute-Corse", "Haute-Garonne", "Haute-Loire", "Haute-Marne", "Hautes-Alpes", "Haute-Saône", "Haute-Savoie", "Hautes-Pyrénées", "Haute-Vienne", "Haut-Rhin", "Hauts-de-Seine", "Hérault", "Ille-et-Vilaine", "Indre", "Indre-et-Loire", "Isère", "Jura", "Landes", "Loire", "Loire-Atlantique", "Loiret", "Loir-et-Cher", "Lot", "Lot-et-Garonne", "Lozère", "Maine-et-Loire", "Manche", "Marne", "Mayenne", "Meurthe-et-Moselle", "Meuse", "Morbihan", "Moselle", "Nièvre", "Nord", "Oise", "Orne", "Paris", "Pas-de-Calais", "Puy-de-Dôme", "Pyrénées-Atlantiques", "Pyrénées-Orientales", "Rhône", "Saône-et-Loire", "Sarthe", "Savoie", "Seine-et-Marne", "Seine-Maritime", "Seine-St-Denis", "Somme", "Tarn", "Tarn-et-Garonne", "Territoire-de-Belfort", "Val-de-Marne", "Val-d'Oise", "Var", "Vaucluse", "Vendée", "Vienne", "Vosges", "Yonne", "Yvelines" });
+
+    // Use this for initialization
+    void Start()
+    {
+
+        foreach (string location in locations)
+        {
+            GameObject item = Instantiate(toggle);
+            item.transform.parent = content.transform;
+
+
+            item.transform.localRotation = toggle.transform.localRotation;
+            item.transform.localScale = toggle.transform.localScale;
+            item.transform.localPosition = Vector3.zero;
+
+            item.name = location;
+            GameObject label = item.transform.Find("Label").gameObject;
+            var labelText = label.GetComponent<Text>();
+            labelText.text = location;
+        }
+        toggle.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/InitializeSearchSkills.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitializeSearchSkills : MonoBehaviour {
+
+    public GameObject content;
+    public GameObject toggle;
+
+    private static ArrayList skills = new ArrayList(new string[] {"PHP", "Java", "Javascript", "Comptabilité", "C++", "Gestion", "SQL", "MongoDB", "UnitTest", "Design Pattern", "Relationnel", "NodeJs", "Unity", "Réalité Virtuelle", "Réalité Augmentée", "Swift", "Objective-C", "Android", "Kotlin", "Méthode Agile", "Symfony", "HTML/CSS", "React", "Dart", "Flutter", "Xamarin", "C#", "Travail d'équipe", "Ergonomie", "Marketing", "Design", "Embarqué", "Linux",
+    "DevOps", "Cubernets", "Docker", "Machine Learning", "Deep Learning" });
+
+    // Use this for initialization
+    void Start () {
+       
+        foreach (string skill in skills)
+        {
+            GameObject item = Instantiate(toggle);
+            item.transform.parent = content.transform;
+
+            item.transform.localRotation = toggle.transform.localRotation;
+            item.transform.localScale = toggle.transform.localScale;
+            item.transform.localPosition = Vector3.zero;
+
+            item.name = skill;
+            GameObject label = item.transform.Find("Label").gameObject;
+            var labelText = label.GetComponent<Text>();
+            labelText.text = skill;
+        }
+        toggle.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
