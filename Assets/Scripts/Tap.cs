37a0180ea08e8cbef08c8dd0bd1e7b5a9d54fcef
jmodoux <josua.modoux@gmail.com>
2018-06-04 09:29:05 +0200

    samsung gear control (tap)

---
+++ Assets/Scripts/Tap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour {
+    public GameObject obj;
+	// Use this for initialization
+	void Start () {
+      //  OVRTouchpad.Create();
+      //  OVRTouchpad.TouchHandler += HandleTouchHandler;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetButton("Tap"))
+        {
+            // Do something if tap starts
+        }
+
+        if (Input.GetButtonUp("Tap"))
+        {
+            obj.transform.Rotate(new Vector3(1, 2, 5));
+        }
+        /*
+        if (touchArgs.TouchType == OVRTouchpad.TouchEvent.Right)
+        {
+            text2.text = "Right!";
+
+        }
+        if (touchArgs.TouchType == OVRTouchpad.TouchEvent.Left)
+        {
+            text2.text = "Left!";
+
+        }*/
+    }
+
+}
